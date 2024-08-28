c7bf2ffb72e2bbc60b6606b94ba393217ef74118
Josueidiaquez <josueidiaquez4@gmail.com>
2024-08-28 13:50:25 -0600

    Update Program.cs

--- Proyecto-descuento-cantidad/Program.cs
+++ Proyecto-descuento-cantidad/Program.cs
@@ -13,6 +13,38 @@ class Program
             Console.WriteLine("2. Capacitores");
             Console.WriteLine("3. Leds");
             Console.WriteLine("4. Salir");
+            Console.WriteLine("=======================");
+            Console.WriteLine("Seleccione una Opcion: ");
+
+            string opcion = Console.ReadLine();
+
+            switch (opcion)
+            {
+                case "1":
+                    Console.WriteLine("Has seleccionado la opcion: 1.");
+                    break;
+                case "2":
+                    Console.WriteLine("Has seleccionado la opcion: 2.");
+                    break;
+                case "3":
+                    Console.WriteLine("Has seleccionado la opcion: 3.");
+                    break;
+                case "4":
+                    Console.WriteLine("Saliendo del programa...");
+                    continuar = false;
+                    break;
+
+                default:
+                    Console.WriteLine("opcion no valida, intente de nuevo");
+                    break;
+
+                if (continuar)
+                {
+                    Console.WriteLine("Presione cualquier tecla para continuar...");
+                    Console.ReadKey();
+                }
+                    
+            }
         }
     }
 }
