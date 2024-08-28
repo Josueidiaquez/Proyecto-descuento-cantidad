71a827ed6df76208eadff31f93307e367ade98bb
Josueidiaquez <josueidiaquez4@gmail.com>
2024-08-28 13:28:04 -0600

    actualizacion

--- Proyecto-descuento-cantidad/Program.cs
+++ Proyecto-descuento-cantidad/Program.cs
@@ -1,1 +1,18 @@
 ﻿using System;
+class Program
+{
+    static void main(string[] args)
+    {
+        bool continuar = true;
+
+        while (continuar)
+        {
+            Console.Clear();
+            Console.WriteLine("==== Ventas Varias ====");
+            Console.WriteLine("1. Transistores");
+            Console.WriteLine("2. Capacitores");
+            Console.WriteLine("3. Leds");
+            Console.WriteLine("4. Salir");
+        }
+    }
+}
