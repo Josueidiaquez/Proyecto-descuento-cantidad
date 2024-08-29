542105a1b80db6860785c2e5c07a757a3e1bc17e
Josueidiaquez <josueidiaquez4@gmail.com>
2024-08-29 11:31:28 -0600

    Update Program.cs

--- Proyecto-descuento-cantidad/Program.cs
+++ Proyecto-descuento-cantidad/Program.cs
@@ -44,7 +44,45 @@ class Program
             {
                 Console.WriteLine("Presione cualquier tecla para continuar...");
                 Console.ReadKey();
-            
+
+            }
+
+            while (continuar)
+            {
+                Console.WriteLine("==== Categorias ====");
+                Console.WriteLine("1. Bebidas");
+                Console.WriteLine("2. Ropa");
+                Console.WriteLine("3. Joyeria");
+                Console.WriteLine("4. Cajas de regalo");
+                Console.WriteLine("5. velas");
+                Console.WriteLine("6. Salir");
+                Console.WriteLine("====================");
+                Console.WriteLine("Seleccione la categoria: ");
+
+                string categoria = Console.ReadLine();
+
+                switch (categoria) 
+                {
+                    case "1":
+                        Console.WriteLine("Has seleccionado la opcion 1.");
+                        break;
+                    case "2":
+                        Console.WriteLine("Has seleccionado la opcion 2.");
+                        break;
+                    case "3":
+                        Console.WriteLine("Has seleccionado la opcion 3.");
+                        break;
+                    case "4":
+                        Console.WriteLine("Has seleccionado la opcion 4.");
+                        break;
+                    case "5":
+                        Console.WriteLine("Has seleccionado la opcion 5.");
+                        break;
+                    case "6":
+                        Console.WriteLine("Saliendo del programa...");
+                        continuar = false;
+                        break;
+                }
             }
         }
     }
